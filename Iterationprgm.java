c678b74d7da681042969c096922bcad914f16ac9
Divyasri0715 <88181037+Divyasri0715@users.noreply.github.com>
2021-07-30 03:45:25 -0400

    Create Iterationprgm.java

---
+++ Iterationprgm.java
@@ -0,0 +1,16 @@
+public class IterationDemo {
+
+	public static void main(String[] args) {
+		
+//Iteration---- while, do while, for, for each loop
+		
+		for (int i=1;i<=5;i++)
+		{
+			System.out.println("hello");
+		
+		
+		}
+	}
+
+
+}
